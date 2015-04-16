0f6066adf6a6ed684706154f75a10c3abf4ffcb3
Alex Revell & kirstylm <pair+alexrevell.kirstylm@devbootcamp.com>
2015-04-16 16:00:37 +1200

    added password encryption in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
 
   has_many :tweets
+
+  before_save do
+    encrypt_password
+  end
+
+  def encrypt_password
+    self.password = SCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    if user = User.find_by(email: email)
+      return user if SCrypt::Password.new(user.password) == password
+    else raise "Incorrect email or password or password and email or something"
+    end
+  end
+
 end
